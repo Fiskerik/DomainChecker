5c3c30c52ce3291efef038ec571a7e0aaf0da2ae
Fiskerik <63838726+Fiskerik@users.noreply.github.com>
2026-02-10 09:14:50 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,18 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'DropWatch - Hitta utgående domäner',
-  description: 'Bevaka domäner i Pending Delete-stadiet'
+  description: 'Bevaka domäner i Pending Delete-stadiet',
+  // Lägg till verifiering här
+  verification: {
+    other: {
+      'impact-site-verification': 'ca0aaced-7984-4e30-a189-511ccce10bb0',
+    },
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="sv">
-      
       <body className={inter.className}>{children}</body>
     </html>
   );
